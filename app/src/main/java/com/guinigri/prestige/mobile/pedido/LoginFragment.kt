0e2a809d54713a3f6e4bb7f8cc0edbb430d62653
Guilherme Nigri <guilhermetnigri@gmail.com>
2021-07-18 23:05:08 -0300

    Criar o layout da tela de login e criar as funções dos botoes da home e login;

--- app/src/main/java/com/guinigri/prestige/mobile/pedido/LoginFragment.kt
+++ app/src/main/java/com/guinigri/prestige/mobile/pedido/LoginFragment.kt
@@ -1,29 +1,29 @@
 package com.guinigri.prestige.mobile.pedido
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
-
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState);
 
-        btn_login.setOnClickListener {
-            findNavController().navigate(R.id.loginFragment);
+        btn_return.setOnClickListener {
+            findNavController().navigate(R.id.homeFragment);
         }
     }
 
